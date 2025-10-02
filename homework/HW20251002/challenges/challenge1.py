f2afc24982b8cd945ac84d349091128f4ce01cf9
Taylor C. Powell <taylorp661@gmail.com>
2025-10-02 15:32:52 -0400

    Finish challenge1 draft 1

--- homework/HW20251002/challenges/challenge1.py
+++ homework/HW20251002/challenges/challenge1.py
@@ -16,8 +16,19 @@ class Challenge1:
         self.meta_dat = open(meta_dat_path, 'r').readlines()
         self.gene_dat = open(gene_dat_path, 'r').readlines()
 
+    def compile_metdat_accesions(self, met_dat: list) -> list:
+        return [line.split(",")[3] for line in met_dat[1:]]
+    
+    def compile_genome_accessions(self, gene_dat: list) -> list:
+        return [line for line in gene_dat if line.startswith(">")]
+
     def find_common_accessions(self):
-        print(self.gene_dat)
+        meta_accessions = self.compile_metdat_accesions(self.meta_dat)
+        genome_accessions = self.compile_genome_accessions(self.gene_dat)
+        common_accessions = [acc for acc in meta_accessions if any(acc in g_acc for g_acc in genome_accessions)]
+        print("Common accessions found:")
+        for acc in common_accessions:
+            print(acc)
 
 if __name__ == "__main__":
-    Challenge1("data/Challenge1_meta.dat", "data/Challenge1_gene.dat").find_common_accessions()
+    Challenge1("homework\HW20251002\data\metadata.csv", "homework\HW20251002\data\genome.fasta").find_common_accessions()
