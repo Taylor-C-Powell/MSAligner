5dc9c8e1ba47e432b10428f4d7cc6f2b72a1fad3
Taylor C. Powell <taylorp661@gmail.com>
2025-10-02 15:41:23 -0400

    Finish challenge 1

--- homework/HW20251002/challenges/challenge1.py
+++ homework/HW20251002/challenges/challenge1.py
@@ -16,19 +16,27 @@ class Challenge1:
         self.meta_dat = open(meta_dat_path, 'r').readlines()
         self.gene_dat = open(gene_dat_path, 'r').readlines()
 
-    def compile_metdat_accesions(self, met_dat: list) -> list:
-        return [line.split(",")[3] for line in met_dat[1:]]
-    
-    def compile_genome_accessions(self, gene_dat: list) -> list:
-        return [line for line in gene_dat if line.startswith(">")]
+    def compile_metdat_accesions(self) -> list:
+        return [line.split(",")[3] for line in self.meta_dat[1:]]
+
+    def compile_genome_accessions(self) -> list:
+        return [line for line in self.gene_dat if line.startswith(">")]
 
     def find_common_accessions(self):
-        meta_accessions = self.compile_metdat_accesions(self.meta_dat)
-        genome_accessions = self.compile_genome_accessions(self.gene_dat)
+        meta_accessions = self.compile_metdat_accesions()
+        genome_accessions = self.compile_genome_accessions()
         common_accessions = [acc for acc in meta_accessions if any(acc in g_acc for g_acc in genome_accessions)]
-        print("Common accessions found:")
-        for acc in common_accessions:
-            print(acc)
+        return common_accessions
+    
+    def csv_output(self, output_path: str):
+        with open(output_path, 'w') as out_file:
+            out_file.write("Common Accessions\n")
+            for acc in self.find_common_accessions():
+                out_file.write(f"{acc}\n")
 
 if __name__ == "__main__":
-    Challenge1("homework\HW20251002\data\metadata.csv", "homework\HW20251002\data\genome.fasta").find_common_accessions()
+    met = "homework\HW20251002\data\metadata.csv"
+    gen = "homework\HW20251002\data\genome.fasta"
+    out_path = "homework\HW20251002\challenges\challenge1_output.csv"
+    c1 = Challenge1(met, gen)
+    c1.csv_output(out_path)
