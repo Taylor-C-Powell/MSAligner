083308c30be7b077811bb1fad0183d9e1a072342
Taylor C. Powell <taylorp661@gmail.com>
2025-10-02 15:42:29 -0400

    Comments for challenge 1

--- homework/HW20251002/challenges/challenge1.py
+++ homework/HW20251002/challenges/challenge1.py
@@ -8,6 +8,7 @@ class Challenge1:
     A class to encapsulate the solution for Challenge 1.
     """
 
+    # initialize class object with paths to data files
     def __init__(self, meta_dat_path: str, gene_dat_path: str):
         """
         Initialize the challenge with input data.
@@ -16,24 +17,28 @@ class Challenge1:
         self.meta_dat = open(meta_dat_path, 'r').readlines()
         self.gene_dat = open(gene_dat_path, 'r').readlines()
 
+    # Compile list of accessions from metadata and genome data
     def compile_metdat_accesions(self) -> list:
         return [line.split(",")[3] for line in self.meta_dat[1:]]
 
     def compile_genome_accessions(self) -> list:
         return [line for line in self.gene_dat if line.startswith(">")]
 
+    # Find common accessions between metadata and genome data
     def find_common_accessions(self):
         meta_accessions = self.compile_metdat_accesions()
         genome_accessions = self.compile_genome_accessions()
         common_accessions = [acc for acc in meta_accessions if any(acc in g_acc for g_acc in genome_accessions)]
         return common_accessions
     
+    # Output common accessions to CSV file
     def csv_output(self, output_path: str):
         with open(output_path, 'w') as out_file:
             out_file.write("Common Accessions\n")
             for acc in self.find_common_accessions():
                 out_file.write(f"{acc}\n")
 
+# Main function to run the challenge
 if __name__ == "__main__":
     met = "homework\HW20251002\data\metadata.csv"
     gen = "homework\HW20251002\data\genome.fasta"
