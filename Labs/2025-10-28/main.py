bd06514dcefcb8288bd8e5279b28b01d8ac7c63d
Taylor C. Powell <taylorp661@gmail.com>
2025-10-23 17:16:51 -0400

    Finished

--- Labs/2025-10-28/main.py
+++ Labs/2025-10-28/main.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
 import pandas as pd
-import numpy as np
 from typing import Any
 from sequence_functions.read import read_fasta
+from sequence_functions.part2_nuc_to_aa import nuc_to_aa
 from sequence_functions.part1_MaxNucCounts import *
 
 class Biopython_demo:
     """
-    This class demonstrates reading a FASTA file and counting nucleotides.
+    This class demonstrates reading a FASTA file and counting nucleotides and
+    converting nucleotide sequences to amino acid sequences.
     It uses pandas and numpy for data manipulation.
     """
 
@@ -19,9 +20,10 @@ class Biopython_demo:
         Parameters
         ----------
         fasta_file : str
-            Path to the input fasta file containing sequences to align.
+            Path to the input fasta file containing sequences.
         """
         self.fasta_file = fasta_file
+        data = read_fasta(self.fasta_file)
         self.output_file = output_file
 
     def find_max_nuc_counts(self) -> list:
@@ -44,21 +46,35 @@ class Biopython_demo:
         max_nucleotides = [nuc for nuc, count in nucleotide_counts.items() if count == max_count]
 
         return max_nucleotides, max_count
+    
+    def nuc_to_aa(self) -> str:
+        """
+        Convert nucleotide sequences to amino acid sequences.
+        """
+        fasta_sequences = parse_fasta(self.fasta_file)
+        amino_acid_sequences = {}
+        for seq_name, nuc_sequence in fasta_sequences.items():
+            amino_acid_sequences[seq_name] = nuc_to_aa(nuc_sequence)
+        return amino_acid_sequences
 
 
 def main() -> None:
     """
-    Main function to execute the alignment workflow.
+    Main function to execute the workflow.
 
     Returns
     -------
     None
     """
     import sys
-    runner = Biopython_demo(fasta_file="sequences.fasta", output_file="output.csv")
+    runner = Biopython_demo(fasta_file="Labs/2025-10-28/sequences.fasta", output_file="Labs/2025-10-28/output.csv")
     sequences = parse_fasta(runner.fasta_file)
+    sys.stdout.write("Part1: Finding Maximum Nucleotide Counts\n")
     for seq in sequences:
         sys.stdout.write(f"The most frequent nucleotide from {seq} is {runner.find_max_nuc_counts()[0]} with a count of {runner.find_max_nuc_counts()[1]}\n")
+    sys.stdout.write("Part2: Converting nuclotide sequences to amino acid sequences\n")
+    for seq in sequences:
+        sys.stdout.write(f"The amino acid sequence for {seq} is {runner.nuc_to_aa()}\n")
 
 
 if __name__ == "__main__":
